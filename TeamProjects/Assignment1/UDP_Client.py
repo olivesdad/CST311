82c9a57dceca40db43f3e403a9437312f77fc68b
andy <ashiraki@csumb.edu>
2022-05-02 21:44:55 -0700

    update

--- TeamProjects/Assignment1/UDP_Client.py
+++ TeamProjects/Assignment1/UDP_Client.py
@@ -1,7 +1,7 @@
 from email import message
 from socket import *
-serverName = '127.0.0.1'
-serverPort = 3033
+serverName = '10.0.0.2'
+serverPort = 12000
 clientSocket = socket(AF_INET, SOCK_DGRAM)
 mess = input("input lowecase sentence: ")
 
