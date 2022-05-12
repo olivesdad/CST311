0d3814e320075f50a14903fd5bd50293fdcde6e5
andy <ashiraki@csumb.edu>
2022-05-11 22:43:03 -0700

    remove fstring

--- TeamProjects/Assignment2/pingaroo.py
+++ TeamProjects/Assignment2/pingaroo.py
@@ -16,11 +16,11 @@ def printSummary(hit, pings, rtt):
     try:
         loss = ((pings - hit) / pings)*100 
         rttAvg = rtt / hit
-        print(f'\nRESULTS: \n'
-              f'Attempts:    {pings}\n'
-              f'Responsees:  {hit}\n'
-              f'Loss rate:   {round(loss,2)}%\n'
-              f'Average RTT: {round(rttAvg,2)}ms')
+        print('\nRESULTS: \n'
+              'Attempts:    '+str(pings)+'\n'
+              'Responsees:  '+str(hit)+'\n'
+              'Loss rate:   '+str(round(loss,2))+'%\n'
+              'Average RTT: '+str(round(rttAvg,2))+'ms')
     except ZeroDivisionError:
         print("NO RESPONSE!")
 #ping loop
@@ -30,7 +30,7 @@ def pingaroo(ip, port, pings):
     count = 0
     acc = [0.0]
     hit = 0
-    print(f'Ping to: {ip}:{port} Count: {pings}')
+    print('Ping to: '+ str(ip)+':'+str(port)+' Count: '+ str(pings))
 
     while count < pings:
         #try block, get a time stamp, send message wait for response, print response increment hit counter
@@ -38,7 +38,7 @@ def pingaroo(ip, port, pings):
             start = time.perf_counter()
             clientSocket.sendto('s'.encode(), (ip, port))
             modMess, serverAddress = clientSocket.recvfrom(2048)
-            print(f"PING {ip} time={timeDiff(start, time.perf_counter(), acc)}ms") 
+            print("PING "+ str(ip)+" time=" + str(timeDiff(start, time.perf_counter(), acc)) + "ms") 
             hit += 1
         
         #socket throws timeout exception so if we exceet timeout go here and just print timeout
