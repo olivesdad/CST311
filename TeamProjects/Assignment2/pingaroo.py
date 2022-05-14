ce0ce05fd674c4e9b72a0442fd8670d0ef1a642b
andy <ashiraki@csumb.edu>
2022-05-14 16:54:10 -0700

    aded min and max rtt

--- TeamProjects/Assignment2/pingaroo.py
+++ TeamProjects/Assignment2/pingaroo.py
@@ -3,32 +3,47 @@ from email import message
 from socket import *
 import time
 import sys
+timeouts = 1
 numPings = 10
 
 #retrund start end time and add to rtt accumulator 
-def timeDiff(start, end, acc):
-    diff = round(end * 1000 - start * 1000, 2)
-    acc[0]+= diff
+def timeDiff(start, end, rtts):
+    diff = round(end * 1000 - start * 1000, 4)
+    rtts.append(diff)
     return diff
 
 #call at the end to print results of ping test
-def printSummary(hit, pings, rtt):
+def printSummary(hit, pings, rtts):
     try:
-        loss = ((pings - hit) / pings)*100 
-        rttAvg = rtt / hit
+        loss = (len(rtts) / pings)*100 
+        min= timeouts * 1000
+        max = 0
+        sum = 0
+
+        for num in rtts:
+            if num < min:
+                min = num
+            elif num > max:
+                max = num
+            sum += num
+        
+        rttAvg = sum / hit
         print('\nRESULTS: \n'
               'Attempts:    '+str(pings)+'\n'
               'Responsees:  '+str(hit)+'\n'
-              'Loss rate:   '+str(round(loss,2))+'%\n'
-              'Average RTT: '+str(round(rttAvg,2))+'ms')
+              'Loss rate:   '+str(round(loss,4))+'%\n'
+              'Min rtt:     '+str(min)+'ms\n'
+              'Max rtt:     '+str(max)+'ms\n'
+              'Average RTT: '+str(round(rttAvg,4))+'ms')
     except ZeroDivisionError:
         print("NO RESPONSE!")
+
 #ping loop
 def pingaroo(ip, port, pings):
     clientSocket = socket(AF_INET, SOCK_DGRAM)
-    clientSocket.settimeout(1)
+    clientSocket.settimeout(timeouts)
     count = 0
-    acc = [0.0]
+    rtts = []
     hit = 0
     print('Ping to: '+ str(ip)+':'+str(port)+' Count: '+ str(pings))
 
@@ -38,7 +53,7 @@ def pingaroo(ip, port, pings):
             start = time.perf_counter()
             clientSocket.sendto('s'.encode(), (ip, port))
             modMess, serverAddress = clientSocket.recvfrom(2048)
-            print("PING "+ str(ip)+" time=" + str(timeDiff(start, time.perf_counter(), acc)) + "ms") 
+            print("PING "+ str(ip)+" time=" + str(timeDiff(start, time.perf_counter(), rtts)) + "ms") 
             hit += 1
         
         #socket throws timeout exception so if we exceet timeout go here and just print timeout
@@ -48,7 +63,7 @@ def pingaroo(ip, port, pings):
         count +=1
 
     #do the summary thing
-    printSummary(hit , count, round(acc[0],2) )
+    printSummary(hit , count, rtts) 
     clientSocket.close()
 
 if __name__ == '__main__':
